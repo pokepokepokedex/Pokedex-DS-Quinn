28c6e21b6dcdcd1de6ecf497494960308c290692
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-03-11 20:11:37 -0400

    prototype done

---
+++ models.py
@@ -0,0 +1,26 @@
+import sqlite3 as sl
+import pandas as pd  # type: ignore
+
+
+def clean_lite_6(datf: pd.DataFrame) -> pd.DataFrame:
+    return (datf.fillna('')
+            .assign(Legendary=[1 if x else 0 for x in datf.Legendary],
+                    Sp_Attack=datf['Sp. Atk'],
+                    Sp_Defense=datf['Sp. Def'],
+                    Type1=datf['Type 1'],
+                    Type2=datf['Type 2'])
+            .drop(['Sp. Atk', 'Sp. Def', 'Type 1', 'Type 2'], axis=1)
+            .rename(lambda s: s.lower() + '_g6', axis='columns')
+            )
+
+
+df6 = pd.read_csv('Pokemon.csv').pipe(clean_lite_6)
+
+df7 = pd.read_csv('pokemon_w7.csv')
+
+df = df7.merge(df6, how='outer', left_on='name', right_on='name' + '_g6')
+
+
+#conn = sl.connect('pokemon7.sqlite3')
+
+#df.to_sql('pokemon', conn)
